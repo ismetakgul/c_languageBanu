6649d8cf6868fdeac6a3ef079738c31db478f93f
ismetakgul <99439215+ismetakgul@users.noreply.github.com>
2023-01-06 11:54:31 +0300

    Update hata_bul.c

--- Odevler/hata_bul.c
+++ Odevler/hata_bul.c
@@ -1,16 +1,17 @@
-#include <stdion.h>
-int main()
+#include <stdio.h>
+
+int main() 
 {
-    char name[0][10], i
-    printf("Enter the 3 name :\n");
-    for (i = 0; i < 3; i++)
-    {
-        printf("Enter the sring :");
-        scanf("%s", &name[i]);
-    }
-    for (i = 0; i < 3; i++)
-    {
-        printf("%s\n", &name[i]);
-    }
-    return 10;
+  char name[3][10], i;
+  printf("3 isim girin:\n");
+  for (i = 0; i < 3; i++) 
+  {
+    printf("İsim girin: ");
+    scanf("%s", &name[i]);
+  }
+  for (i = 0; i < 3; i++) 
+  {
+    printf("%s\n", name[i]);
+  }
+  return 0;
 }
