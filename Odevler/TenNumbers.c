7b3fd6dd229279f597eefb8eb5164b4ee11eb23e
ismetakgul <99439215+ismetakgul@users.noreply.github.com>
2023-01-06 11:45:10 +0300

    Update TenNumbers.c

--- Odevler/TenNumbers.c
+++ Odevler/TenNumbers.c
@@ -5,5 +5,19 @@
 int main()
 {
 //   YOUR CODE HERE
+  int numbers[10];  // dizi tanımla
   
+  printf("Lütfen 10 sayı girin:\n");
+  for (int i = 0; i < 10; i++)
+  {
+    scanf("%d", &numbers[i]);  // kullanıcıdan sayı girişi al
+  }
+
+  printf("Girdiğiniz sayılar:\n");
+  for (int i = 0; i < 10; i++) 
+  {
+    printf("%d ", numbers[i]);  // sayıları ekrana yazdır
+  }
+
+  return 0;
 }
